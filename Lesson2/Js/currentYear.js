4b4db8adcf27c2496a805b5a04eb55861baa9c9a
chadbrizuela.github.io <bchadgazelem@gmail.com>
2021-01-13 14:42:32 +0800

    created desing principle.html and of its css

--- Lesson2/Js/currentYear.js
+++
@@ -1,5 +0,0 @@
-
-let currentYear = new Date().getFullYear();
-let lastUpdated = document.lastModified;
-document.getElementById("last-update").textContent = lastUpdated;
-document.getElementById("current-year").textContent = currentYear;
