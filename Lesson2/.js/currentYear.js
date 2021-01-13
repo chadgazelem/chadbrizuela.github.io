9dbfe5670e04b538f131fbeb512bacfc10f19995
chadbrizuela.github.io <bchadgazelem@gmail.com>
2021-01-13 16:05:27 +0800

    finish the viewport of a mobile

---
+++ Lesson2/.js/currentYear.js
@@ -0,0 +1,5 @@
+
+let currentYear = new Date().getFullYear();
+let lastUpdated = document.lastModified;
+document.getElementById("last-update").textContent = lastUpdated;
+document.getElementById("current-year").textContent = currentYear;
