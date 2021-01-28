46da522299eee9e03398a33dde1d179488984952
chadbrizuela.github.io <bchadgazelem@gmail.com>
2021-01-28 20:36:32 +0800

    finish the head nav and main

---
+++ Lesson4/js/currentYear.js
@@ -0,0 +1,4 @@
+let currentYear = new Date().getFullYear();
+let lastUpdated = document.lastModified;
+document.getElementById("last-update").textContent = lastUpdated;
+document.getElementById("current-year").textContent = currentYear;
