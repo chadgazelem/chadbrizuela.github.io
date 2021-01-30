b40f969fa44af5fcb9f3414ebb3f7a7d0f501557
chadbrizuela.github.io <bchadgazelem@gmail.com>
2021-01-30 23:01:19 +0800

    1st edit

--- Lesson4/js/currentYear.js
+++ Lesson4/js/currentYear.js
@@ -1,4 +1,2 @@
 let currentYear = new Date().getFullYear();
-let lastUpdated = document.lastModified;
-document.getElementById("last-update").textContent = lastUpdated;
 document.getElementById("current-year").textContent = currentYear;
